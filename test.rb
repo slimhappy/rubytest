6dea70aa913c3cfa33f054cb276b18228b3cc801
slimhappy <767059112@qq.com>
2018-02-12 17:00:58 +0800

    learn some ruby

--- test.rb
+++ test.rb
@@ -1,14 +1,14 @@
-class Helium
-	def to_s
-		"he"
-	end
-	def to_str
-		"helium"
-	end
+class String
+
+  def rot13
+  	self.tr("A-Ma-mN-Zn-z","N-Zn-zA-Ma-m")
+  end
+
 end
 
-e = Helium.new
-print "Element is "
-puts e
-puts "Element is "+ e
-puts "Element is #{e}"
+joke = "Y2K bug"
+joke13 = joke.rot13
+puts joke13
+
+episode_2 = "Fcbvyre: Naanxva qbrfa'g trg xvyyq."
+puts episode_2.rot13
